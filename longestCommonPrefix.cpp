4d7d7f3fcd2a7fc04a8ac2df98844b458c1cae56
vaibhavarora102 <aroravaibhav102@gmail.com>
2021-01-04 22:50:15 +0530

    problem4

---
+++ longestCommonPrefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int count = 0,min = 4000;
+        string str;
+        for(int i=0;i<strs.size();i++){
+            int len =strs[i].size();
+            if (len<min){
+                min =len;
+                str = strs[i];
+            }
+        }
+        
+        for(int i=0;i<min;i++){
+            for(int j=0;j<strs.size();j++){
+                if (strs[j][i]!=str[i]){
+                    return str.substr(0,count);
+                }
+                
+            }
+            count++;
+        }
+        
+        return str.substr(0,count);
+    }
+};
